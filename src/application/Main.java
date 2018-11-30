ad310f4aa3601fe0c20786e73b6962ed3304b019
katayama <yasucom4646@gmail.com>
2018-11-29 19:59:12 -0800

    Refactor

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,13 +6,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.Pokemon;
 
 import java.io.IOException;
 
 public class Main extends Application {
-    Pokemon pokemon = new Pokemon();
-    PokemonContoller pokemonContoller = new PokemonContoller();
 
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -21,34 +18,6 @@ public class Main extends Application {
         root.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         root.getStylesheets().add("https://fonts.googleapis.com/css?family=Press+Start+2P");
         primaryStage.show();
-
-//        HBox hbox = new HBox();
-//        hbox.setAlignment(Pos.CENTER);
-//
-//        Button button = new Button("Button");
-//        Label label = new Label("Enter Pokemon Name");
-//        TextField textField = new TextField();
-//
-//        button.setOnAction((event) -> {
-//            label.setText("");
-//            pokemon = pokemonContoller.serch(textField.getText());
-//            label.setText(
-//                    pokemon.getName() + "\n" +
-//                    pokemon.getImage() + "\n" +
-//                    pokemon.getType1() + "\n" +
-//                    pokemon.getType2() + "\n" +
-//                    pokemon.getRoar());
-//        });
-//
-//        hbox.getChildren().add(button);
-//        hbox.getChildren().add(textField);
-//        hbox.getChildren().add(label);
-//
-//        ((Group) scene.getRoot()).getChildren().add(hbox);
-
-//        primaryStage.setTitle("Button Test");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
     }
     public static void main(String[] args) {
         launch(args);
